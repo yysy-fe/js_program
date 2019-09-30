075f293fdcae2053691e12926e6502fd20099bf7
yysy-fe <468068459@qq.com>
2019-09-30 11:15:38 +0800

    二分查找

--- search/二分查找.js
+++ search/二分查找.js
@@ -4,7 +4,7 @@ Array.prototype.search = function (target) {
   let result = -1;
   let start = 0, end = this.length - 1;
   while (start <= end) {
-    let mid = start + Math.floor((end - start) / 2)
+    let mid = Math.floor((end + start) / 2)
     if (this[mid] === target) {
       result = mid;
       break;
