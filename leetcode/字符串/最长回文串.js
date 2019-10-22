cd7c403593f3cb655eac4f58ff4f97cf34f99def
yysy-fe <468068459@qq.com>
2019-10-22 18:10:51 +0800

    兼容 aa

--- leetcode/字符串/最长回文串.js
+++ leetcode/字符串/最长回文串.js
@@ -6,10 +6,15 @@ const testStr = "baaaabaaaaad";
 
 const fn = str => {
   let result = '';  
-  for (let i = 0, len = str.length; i < len; i++) {
-    let item = str[i];
+  for (let i = 0, len = str.length; i < len; i = i + 0.5) {
+    let item = str[i] ||'';
     let prev = i - 1;
     let next = i + 1;
+    if (String(i).indexOf('.') > -1) {
+      prev = Math.ceil(i - 1);
+      next = Math.floor(i + 1);
+    }
+    
     while (str[prev] === str[next] && str[prev] !== undefined) {
       item = str[prev] + item + str[next];
       prev--;
